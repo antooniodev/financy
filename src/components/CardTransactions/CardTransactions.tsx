e0a88041381cfd195cf8f6ab4765fbff609bf762
4ntonio19 <vgoncalvescarvalho@outlook.com>
2025-02-18 01:56:53 -0300

    chore: clear code

---
+++ src/components/CardTransactions/CardTransactions.tsx
@@ -0,0 +1,32 @@
+import { useNavigate } from 'react-router-dom'
+import { ContainerTransactions } from './styles'
+
+type Props = {
+  title: string
+  explanation: string
+  icon: React.ReactNode
+  bgdIconColor?: string
+  navigateTo: string
+}
+const CardTransactions = ({
+  title,
+  explanation,
+  icon,
+  bgdIconColor,
+  navigateTo,
+}: Props) => {
+  const navigate = useNavigate()
+  return (
+    <ContainerTransactions onClick={() => navigate(navigateTo)}>
+      <div className="container-icon" style={{ backgroundColor: bgdIconColor }}>
+        {icon}
+      </div>
+      <section className="container-text">
+        <span>{title}</span>
+        <p>{explanation}</p>
+      </section>
+    </ContainerTransactions>
+  )
+}
+
+export default CardTransactions
