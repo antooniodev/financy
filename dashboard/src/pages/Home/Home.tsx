31e49210fc52740574c4725e1d85fd07416d6f32
antooniodev <vgoncalvescarvalho@outlook.com>
2025-03-16 21:45:00 -0300

    chore: add vercel config and get user name

--- dashboard/src/pages/Home/Home.tsx
+++ dashboard/src/pages/Home/Home.tsx
@@ -1,68 +1,77 @@
-import FilterPeriod from '../../components/Filter/FilterPeriod'
-import { ContainerHome, SSection } from './styles'
-import CardTransactions from '../../components/CardTransactions'
-import CategoryChart from '../../components/SummaryCard'
-import TransactionsList from '../../components/TransactionsList'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { IconProp } from '@fortawesome/fontawesome-svg-core'
+import FilterPeriod from "../../components/Filter/FilterPeriod"
+import { ContainerHome, SSection } from "./styles"
+import CardTransactions from "../../components/CardTransactions"
+import CategoryChart from "../../components/SummaryCard"
+import TransactionsList from "../../components/TransactionsList"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { IconProp } from "@fortawesome/fontawesome-svg-core"
 
-import { Outlet } from 'react-router-dom'
-import CardIndicatorsList from '../../components/CardIndicatorsList/CardIndicatorsList'
-import { useGetMonthlyGoalQuery } from '../../services/userService'
-import { formatCurrency } from '../../utils/formatterCurrency'
+import { Outlet } from "react-router-dom"
+import CardIndicatorsList from "../../components/CardIndicatorsList/CardIndicatorsList"
+import { useGetMonthlyGoalQuery } from "../../services/userService"
+import { formatCurrency } from "../../utils/formatterCurrency"
+import { useSelector } from "react-redux"
+import { selectUserState } from "../../store/auth/authSlice"
 const Home = () => {
   // const transactions = useSelector(selectTransactions)
   const { data } = useGetMonthlyGoalQuery()
+  const user = useSelector(selectUserState)
   return (
     <>
       <ContainerHome>
-        <div className="home-content">
-          <header className="home-header">
-            <h1>Olá, Antonio!</h1>
-            <section className="container-period">
+        <div className='home-content'>
+          <header className='home-header'>
+            <h1>
+              Olá, {user.firstName !== "" ? user.firstName : "seja bem-vindo"}!
+            </h1>
+            <section className='container-period'>
               <FilterPeriod />
             </section>
           </header>
           <CardIndicatorsList />
           <SSection>
             <CardTransactions
-              navigateTo="/dashboard/monthlyGoal"
-              bgdIconColor="#F9FAFB"
-              title="Sua meta mensal"
-              explanation={`${formatCurrency(data?.totalOfExpenses ?? 0)} de ${formatCurrency(data?.monthlyGoal ?? 0)} | ${(data?.percentageOfExpenses ?? 0).toFixed(2)}%`}
+              navigateTo='/dashboard/monthlyGoal'
+              bgdIconColor='#F9FAFB'
+              title='Sua meta mensal'
+              explanation={`${formatCurrency(
+                data?.totalOfExpenses ?? 0
+              )} de ${formatCurrency(data?.monthlyGoal ?? 0)} | ${(
+                data?.percentageOfExpenses ?? 0
+              ).toFixed(2)}%`}
               icon={
                 <FontAwesomeIcon
-                  color="#155EEF"
-                  icon={'fa-solid fa-arrow-right-arrow-left' as IconProp}
+                  color='#155EEF'
+                  icon={"fa-solid fa-arrow-right-arrow-left" as IconProp}
                 />
               }
             />
             <CardTransactions
-              navigateTo="/dashboard/addIncome"
-              bgdIconColor="#DCFAE6"
-              title="Adicionar entrada"
-              explanation="Crie manualmente uma entrada"
+              navigateTo='/dashboard/addIncome'
+              bgdIconColor='#DCFAE6'
+              title='Adicionar entrada'
+              explanation='Crie manualmente uma entrada'
               icon={
                 <FontAwesomeIcon
-                  color="#0B9055"
-                  icon={'fa-solid fa-plus' as IconProp}
+                  color='#0B9055'
+                  icon={"fa-solid fa-plus" as IconProp}
                 />
               }
             />
             <CardTransactions
-              navigateTo="/dashboard/addExpense"
-              bgdIconColor="#FEE4E2"
-              title="Adicionar saída"
-              explanation="Crie manualmente uma saída"
+              navigateTo='/dashboard/addExpense'
+              bgdIconColor='#FEE4E2'
+              title='Adicionar saída'
+              explanation='Crie manualmente uma saída'
               icon={
                 <FontAwesomeIcon
-                  color="#D92D20"
-                  icon={'fa-solid fa-minus' as IconProp}
+                  color='#D92D20'
+                  icon={"fa-solid fa-minus" as IconProp}
                 />
               }
             />
           </SSection>
-          <SSection className="transactions-section">
+          <SSection className='transactions-section'>
             <CategoryChart />
             <TransactionsList />
           </SSection>
--- dashboard/src/store/auth/authSlice.ts
+++ dashboard/src/store/auth/authSlice.ts
@@ -1,40 +1,40 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit'
-import { ISessionResponse } from '../../entitites'
-import { api } from '../../services/api'
-import { RootState } from '../store'
+import { createSlice, PayloadAction } from "@reduxjs/toolkit"
+import { ISessionResponse } from "../../entitites"
+import { api } from "../../services/api"
+import { RootState } from "../store"
 
 const initialState: ISessionResponse = {
-  token: '',
+  token: "",
   user: {
-    userId: '',
-    firstName: '',
+    userId: "",
+    firstName: "",
     monthlyGoal: 0,
   },
 }
 export const authSlice = createSlice({
-  name: 'auth',
+  name: "auth",
   initialState,
   reducers: {
-    clearSession: state => {
-      state.token = ''
+    clearSession: (state) => {
+      state.token = ""
       state.user = {
-        userId: '',
-        firstName: '',
+        userId: "",
+        firstName: "",
         monthlyGoal: 0,
       }
       localStorage.clear()
       sessionStorage.clear()
-      window.location.href = '/login'
+      window.location.href = "/login"
     },
   },
-  extraReducers: builder => {
+  extraReducers: (builder) => {
     builder.addMatcher(
       api.endpoints.login.matchFulfilled,
       (state, { payload }: PayloadAction<ISessionResponse>) => {
         state.token = payload.token
         state.user = payload.user
-        localStorage.setItem('token', payload.token)
-        localStorage.setItem('user', JSON.stringify(payload.user))
+        localStorage.setItem("token", payload.token)
+        localStorage.setItem("user", JSON.stringify(payload.user))
       }
     )
   },
